b365c5ee0a53018b3880b8ce27df6595f8f54344
Bruno Santos <brunohqsantos@hotmail.com>
2020-07-28 22:42:11 -0300

    Estrutura Cadastro, carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Logo from "../../assets/img/Logo.png";
 import Button from "../../components/Button";
 import "./styles.css";
+import { Link } from "react-router-dom";
 
 function Menu(){
   return(
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img src={Logo} alt="AluraFlix" className="Logo"/>
-      </a>
+      </Link>
 
-      <Button as="a" href="/" className="ButtonLink">
+      <Button as={Link} to="/cadastro/video" className="ButtonLink">
         Novo Vídeo
       </Button>
     </nav>
